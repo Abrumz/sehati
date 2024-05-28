18967b3694c73058fa519b679c18f02436ed3d99
Made Althaaf Naufal Gusendra <88234890+Abrumz@users.noreply.github.com>
2024-05-28 23:34:49 +0700

    Add mental health fitur

---
+++ mental-health/index.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Sehati</title>
+	<link rel="icon" href="../img/LogoSehati.png">
+
+	<!-- font -->
+	<link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
+	<!-- end font -->
+
+	<link rel="stylesheet" href="../css/bootstrap.css">
+	<link rel="stylesheet" href="../css/ionicons.min.css">
+	<link rel="stylesheet" href="../css/magnific-popup.css">
+	<link rel="stylesheet" href="../css/style.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+	<!-- loader -->
+	<div class="fakeLoader"></div>
+	<!-- end loader -->
+	
+	<!-- navbar -->
+
+	<?php include 'header.html'; ?>
+	
+	<!-- end navbar -->
+
+    <div id="home" class="home-intro" style="padding: 175px 0px 0px;">
+		<div class="container-mental-health">
+			<div class="mental-health-row">
+				<div class="mental-health-text">
+					<div class="home-intro-img">
+						<img src="../img/medical.png" alt="Image">
+					</div>
+					<h1 class="mb-4">Mental Health</h1>
+				</div>
+				<p class="mb-4" style="margin-top: 16px; margin-bottom: 0 !important;">
+					Skrining online adalah salah satu metode tercepat dan termudah untuk mengetahui apakah Anda mengalami gejala masalah kesehatan mental. Kondisi kesehatan mental, seperti <span style="font-weight: 600;">depresi atau kecemasan</span>, adalah nyata, umum, dan bisa diobati.
+				</p>				
+			</div>
+		</div>
+	</div>
+
+	<div class="accordion">
+		<div class="accordion-item">
+		<button class="accordion-button">Tes Depresi</button>
+			<div class="accordion-content">
+				<p>Bagi orang yang mengalami kesedihan dan keputusasaan yang luar biasa, energi rendah, atau citra diri negatif.</p>
+				<button class="depression-test-button">
+					<a href="depression-test">Ikuti Tes Depresi</a>
+				</button>
+			</div>
+		</div>
+
+        <!-- <div class="accordion-item">
+            <button class="accordion-button">ANXIETY TEST</button>
+            <div class="accordion-content">
+                <p>Description for the Anxiety Test.</p>
+                <a href="#">TAKE ANXIETY TEST</a>
+            </div>
+        </div> -->
+        <!-- <div class="accordion-item">
+            <button class="accordion-button">ADHD TEST</button>
+            <div class="accordion-content">
+                <p>Description for the ADHD Test.</p>
+                <a href="#">TAKE ADHD TEST</a>
+            </div>
+        </div> -->
+        <!-- <div class="accordion-item">
+            <button class="accordion-button">BIPOLAR TEST</button>
+            <div class="accordion-content">
+                <p>Description for the Bipolar Test.</p>
+                <a href="#">TAKE BIPOLAR TEST</a>
+            </div>
+        </div> -->
+        <!-- <div class="accordion-item">
+            <button class="accordion-button">PSYCHOSIS & SCHIZOPHRENIA TEST</button>
+            <div class="accordion-content">
+                <p>For people who feel like their brain is playing tricks on them (seeing, hearing or believing things that don't seem real or quite right).</p>
+                <a href="#">TAKE PSYCHOSIS & SCHIZOPHRENIA TEST</a>
+            </div>
+        </div> -->
+        <!-- <div class="accordion-item">
+            <button class="accordion-button">PTSD TEST</button>
+            <div class="accordion-content">
+                <p>Description for the PTSD Test.</p>
+                <a href="#">TAKE PTSD TEST</a>
+            </div>
+        </div> -->
+        <!-- <div class="accordion-item">
+            <button class="accordion-button">EATING DISORDER TEST</button>
+            <div class="accordion-content">
+                <p>Description for the Eating Disorder Test.</p>
+                <a href="#">TAKE EATING DISORDER TEST</a>
+            </div>
+        </div> -->
+        <!-- <div class="accordion-item">
+            <button class="accordion-button">ADDICTION TEST</button>
+            <div class="accordion-content">
+                <p>Description for the Addiction Test.</p>
+                <a href="#">TAKE ADDICTION TEST</a>
+            </div>
+        </div> -->
+        <!-- <div class="accordion-item">
+            <button class="accordion-button">PARENT TEST: YOUR CHILD’S MENTAL HEALTH</button>
+            <div class="accordion-content">
+                <p>Description for the Parent Test.</p>
+                <a href="#">TAKE PARENT TEST</a>
+            </div>
+        </div> -->
+        <!-- <div class="accordion-item">
+            <button class="accordion-button">YOUTH MENTAL HEALTH TEST</button>
+            <div class="accordion-content">
+                <p>Description for the Youth Mental Health Test.</p>
+                <a href="#">TAKE YOUTH MENTAL HEALTH TEST</a>
+            </div>
+        </div> -->
+        <!-- <div class="accordion-item">
+            <button class="accordion-button">TEST DE DEPRESIÓN</button>
+            <div class="accordion-content">
+                <p>Descripción del Test de Depresión.</p>
+                <a href="#">REALIZAR TEST DE DEPRESIÓN</a>
+            </div>
+        </div> -->
+        <!-- <div class="accordion-item">
+            <button class="accordion-button">TEST DE ANSIEDAD</button>
+            <div class="accordion-content">
+                <p>Descripción del Test de Ansiedad.</p>
+                <a href="#">REALIZAR TEST DE ANSIEDAD</a>
+            </div>
+        </div> -->
+        <!-- <div class="accordion-item">
+            <button class="accordion-button">SELF-INJURY SURVEY</button>
+            <div class="accordion-content">
+                <p>Description for the Self-Injury Survey.</p>
+                <a href="#">TAKE SELF-INJURY SURVEY</a>
+            </div>
+        </div> -->
+        <!-- <div class="accordion-item">
+            <button class="accordion-button">WORKPLACE MENTAL HEALTH SURVEY</button>
+            <div class="accordion-content">
+                <p>Description for the Workplace Mental Health Survey.</p>
+                <a href="#">TAKE WORKPLACE MENTAL HEALTH SURVEY</a>
+            </div>
+        </div> -->
+        <!-- <div class="accordion-item">
+            <button class="accordion-button active">POSTPARTUM DEPRESSION TEST (NEW & EXPECTING PARENTS)</button>
+            <div class="accordion-content" style="display: block; max-height: 200px;">
+                <p>For new & expecting parents who began feeling overwhelming sadness during pregnancy or after their child's birth.</p>
+                <a href="#">TAKE POSTPARTUM DEPRESSION TEST (NEW & EXPECTING PARENTS)</a>
+            </div>
+        </div> -->
+    </div>
+
+    <!-- Footer -->
+	<?php include 'footer.html'; ?>
+
+	<!-- script -->
+	<script src="../js/jquery-3.5.1.min.js"></script>
+	<script src="../js/bootstrap.bundle.min.js"></script>
+	<script src="../js/jquery.filterizr.min.js"></script>
+	<script src="../js/imagesloaded.pkgd.min.js"></script>
+	<script src="../js/magnific-popup.min.js"></script>
+	<script src="../js/contact-form.js"></script>
+	<script src="../js/main.js"></script>
+	<script>
+        document.querySelectorAll('.accordion-button').forEach(button => {
+            button.addEventListener('click', () => {
+                const accordionContent = button.nextElementSibling;
+                button.classList.toggle('active');
+
+                if (button.classList.contains('active')) {
+                    accordionContent.style.display = 'block';
+                    accordionContent.style.maxHeight = accordionContent.scrollHeight + "px";
+                } else {
+                    accordionContent.style.maxHeight = 0;
+                    setTimeout(() => {
+                        accordionContent.style.display = 'none';
+                    }, 300);
+                }
+
+                document.querySelectorAll('.accordion-button').forEach(otherButton => {
+                    if (otherButton !== button) {
+                        otherButton.classList.remove('active');
+                        otherButton.nextElementSibling.style.maxHeight = 0;
+                        setTimeout(() => {
+                            otherButton.nextElementSibling.style.display = 'none';
+                        }, 300);
+                    }
+                });
+            });
+        });
+
+        document.addEventListener('click', (event) => {
+            const isClickInsideAccordion = event.target.closest('.accordion');
+            if (!isClickInsideAccordion) {
+                document.querySelectorAll('.accordion-button').forEach(button => {
+                    button.classList.remove('active');
+                    button.nextElementSibling.style.maxHeight = 0;
+                    setTimeout(() => {
+                        button.nextElementSibling.style.display = 'none';
+                    }, 300);
+                });
+            }
+        });
+    </script>
+
+	<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+</body>
+</html> 
---
+++ mental-health/question-test/skor.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sehati</title>
+    <link rel="icon" href="../img/sehati-vector.png">
+
+    <!-- font -->
+    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <!-- end font -->
+
+    <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="../css/ionicons.min.css">
+    <link rel="stylesheet" href="../css/magnific-popup.css">
+    <!-- <link rel="stylesheet" href="../css/style.css"> -->
+    <link rel="stylesheet" href="../css/style.css">
+<body>
+<div id="result">
+    <div id="home" class="home-intro" style="padding: 175px 0px 0px; ">
+        <div class="container-mental-health" style="border-radius: 16px;
+        background: var(--base-white, #FFF);
+        box-shadow: 0px 0px 8px 0px rgba(0, 0, 0, 0.16);">
+            <div class="mental-health-row" style="display: flex;     flex-direction: column;">
+                <div style="display: flex; flex-direction: row; flex-wrap: nowrap; padding: 32px;">
+                    <h1 class="mb-4" style="margin: 0px; display: flex;flex-direction: row;">Skor Kamu : <span id="score"></span></h1>
+                </div>
+                <div class="skor" style="padding: 32px;">
+                    <p class="mb-4" style="margin-top: 16px;margin-bottom: 0 !important;display: flex; justify-content: center; font-weight: 700;">
+                        Tes yang singkat dan valid secara ilmiah, digunakan oleh tenaga kesehatan.
+                    </p>
+                    <p class="mb-4" style="margin: 0px; display: flex;flex-direction: row; justify-content: center; padding-top: 16px;">
+                        <span id="explanation"></span>
+                    </p>  
+                </div>                       
+            </div>
+        </div>
+    </div>
+</div>
+
+    <div id="result" style="display: none">
+        <!-- <h1>Hasil Test: <span id="test_name"></span></h1> -->
+        <p><span id="score"></span></p>
+        <p><span id="explanation"></span></p>
+    </div>
+
+    <!-- script -->
+    <script src="../js/jquery-3.5.1.min.js"></script>
+    <script src="../js/bootstrap.bundle.min.js"></script>
+    <script src="../js/jquery.filterizr.min.js"></script>
+    <script src="../js/imagesloaded.pkgd.min.js"></script>
+    <script src="../js/magnific-popup.min.js"></script>
+    <script src="../js/contact-form.js"></script>
+    <script src="../js/main.js"></script>
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            const urlParams = new URLSearchParams(window.location.search);
+            const test = urlParams.get('test');
+
+            let testName = sessionStorage.getItem('test_name');
+            let score = sessionStorage.getItem(`${test}_score`);
+            let explanation = sessionStorage.getItem(`${test}_explanation`);
+
+            if (testName && score && explanation) {
+                document.getElementById('test_name').textContent = testName;
+                document.getElementById('score').textContent = score;
+                document.getElementById('explanation').textContent = explanation;
+            } else {
+                document.getElementById('result').innerHTML = "<p>No data found. Please complete the test first.</p>";
+            }
+
+            var height = document.body.scrollHeight;
+            window.parent.postMessage({ type: 'setHeight', height: height }, '*');
+        });
+    </script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+</body>
+</html>

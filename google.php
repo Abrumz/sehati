fa427d3c5b60cd1ca9d03001a8e574b5462bbf8a
Made Althaaf Naufal Gusendra <88234890+Abrumz@users.noreply.github.com>
2024-06-18 17:14:35 +0700

    update google

--- google.php
+++ google.php
@@ -2,8 +2,7 @@
 // session_start();
 
 // Database connection
-// $database = new mysqli("localhost", "root", "", "sehati");
-$database= new mysqli("localhost","ilkomerz_sehati","23Gusendra16","ilkomerz_sehati");
+$database = new mysqli("localhost", "ilkomerz_sehati", "23Gusendra16", "ilkomerz_sehati");
 if ($database->connect_error) {
     die("Connection failed: " . $database->connect_error);
 }
@@ -13,7 +12,6 @@ require_once 'vendor/autoload.php';
 // Google client configuration
 $clientID = '682994088356-0gcvafc2qun5o4o5eisgt2b1v326il1a.apps.googleusercontent.com';
 $clientSecret = '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';
-// $redirectUri = 'http://localhost/sehati/login';
 $redirectUri = 'https://sehati.ilkomerz.biz.id/login';
 
 $client = new Google_Client();
@@ -72,7 +70,9 @@ if (isset($_GET['code'])) {
             echo "User type not found.";
         }
     } else {
-        echo "Error during authentication.";
+        echo "Error during authentication: " . $token['error'];
     }
+} else {
+    echo "No code parameter in URL.";
 }
 ?>

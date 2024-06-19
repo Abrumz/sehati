7024258dc4eb8992de3a4c34c7e63c06e7b3dde2
Made Althaaf Naufal Gusendra <88234890+Abrumz@users.noreply.github.com>
2024-06-19 13:39:14 +0700

    fix dashboard

--- patient/doctors.php
+++ patient/doctors.php
@@ -442,7 +442,7 @@ if($_POST){
                                     <td colspan="4">
                                     <br><br><br><br>
                                     <center>
-                                    <img src="../img/notfound.svg" width="25%">
+                                    <img src="../img/404.gif" width="25%">
                                     
                                     <br>
                                     <p class="heading-main12" style="margin-left: 45px;font-size:20px;color:rgb(49, 49, 49)">We  couldnt find anything related to your keywords !</p>

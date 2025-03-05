2ce84c09bee714d95fa901593fabc91ba4e548af
Abrumz <made16jan03@gmail.com>
2025-03-05 11:29:40 +0700

    Refactor signup.php to improve form handling and validation

--- create-account.php
+++ create-account.php
@@ -1,195 +1,194 @@
+<?php
+session_start();
+
+include("connection.php");
+
+$_SESSION["user"] = "";
+$_SESSION["usertype"] = "";
+
+date_default_timezone_set('Asia/Kolkata');
+$date = date('Y-m-d');
+$_SESSION["date"] = $date;
+
+$error = '';
+
+if ($_POST) {
+   if (!isset($_SESSION['personal']) || empty($_SESSION['personal'])) {
+       header("Location: signup.php");
+       exit();
+   }
+
+   $fname = $_SESSION['personal']['fname'];
+   $lname = $_SESSION['personal']['lname'];
+   $name = $fname . " " . $lname;
+   $address = $_SESSION['personal']['address'];
+   $nic = $_SESSION['personal']['nic'];
+   $dob = $_SESSION['personal']['dob'];
+   
+   $email = filter_var($_POST['newemail'], FILTER_SANITIZE_EMAIL);
+   $tele = filter_var($_POST['tele'], FILTER_SANITIZE_STRING);
+   $newpassword = $_POST['newpassword'];
+   $cpassword = $_POST['cpassword'];
+   
+   if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+       $error = 'Format email tidak valid';
+   }
+   elseif (strlen($newpassword) < 8) {
+       $error = 'Password minimal 8 karakter';
+   }
+   elseif ($newpassword !== $cpassword) {
+       $error = 'Konfirmasi password tidak cocok';
+   }
+   else {
+       $stmt = $database->prepare("SELECT * FROM webuser WHERE email = ?");
+       $stmt->bind_param("s", $email);
+       $stmt->execute();
+       $result = $stmt->get_result();
+       
+       if ($result->num_rows > 0) {
+           $error = 'Email sudah terdaftar';
+       } else {
+           $database->begin_transaction();
+           
+           try {
+               $stmt_patient = $database->prepare("INSERT INTO patient (pemail, pname, ppassword, paddress, pnic, pdob, ptel) VALUES (?, ?, ?, ?, ?, ?, ?)");
+               $stmt_patient->bind_param("sssssss", $email, $name, $newpassword, $address, $nic, $dob, $tele);
+               
+               $stmt_webuser = $database->prepare("INSERT INTO webuser (email, usertype) VALUES (?, 'p')");
+               $stmt_webuser->bind_param("s", $email);
+               
+               $patient_result = $stmt_patient->execute();
+               $webuser_result = $stmt_webuser->execute();
+               
+               if ($patient_result && $webuser_result) {
+                   $database->commit();
+                   
+                   $_SESSION["user"] = $email;
+                   $_SESSION["usertype"] = "p";
+                   $_SESSION["username"] = $fname;
+                   
+                   header('Location: patient/index.php');
+                   exit();
+               } else {
+                   throw new Exception("Gagal menyimpan data");
+               }
+           } catch (Exception $e) {
+               $database->rollback();
+               $error = 'Terjadi kesalahan: ' . $e->getMessage();
+           }
+       }
+       
+       $stmt->close();
+   }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/animations.css">  
-    <link rel="stylesheet" href="css/main.css">  
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-
-    <link rel="stylesheet" href="css/signup.css">
-        
-    <title>Create Account</title>
-    
-	<link rel="icon" href="img/LogoSehati.png">
-    <style>
-        .container{
-            animation: transitionIn-X 0.5s;
-        }
-    </style>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="css/animations.css">  
+   <link rel="stylesheet" href="css/main.css">  
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+   <link rel="stylesheet" href="css/signup.css">
+       
+   <title>Buat Akun</title>
+   
+   <link rel="icon" href="img/LogoSehati.png">
 </head>
 <body>
-<?php
-
-//learn from w3schools.com
-//Unset all the server side variables
-
-session_start();
-
-$_SESSION["user"]="";
-$_SESSION["usertype"]="";
-
-// Set the new timezone
-date_default_timezone_set('Asia/Kolkata');
-$date = date('Y-m-d');
-
-$_SESSION["date"]=$date;
-
-
-//import database
-include("connection");
-
-
-
-
-
-if($_POST){
-
-    $result= $database->query("select * from webuser");
-
-    $fname=$_SESSION['personal']['fname'];
-    $lname=$_SESSION['personal']['lname'];
-    $name=$fname." ".$lname;
-    $address=$_SESSION['personal']['address'];
-    $nic=$_SESSION['personal']['nic'];
-    $dob=$_SESSION['personal']['dob'];
-    $email=$_POST['newemail'];
-    $tele=$_POST['tele'];
-    $newpassword=$_POST['newpassword'];
-    $cpassword=$_POST['cpassword'];
-    
-    if ($newpassword==$cpassword){
-        $sqlmain= "select * from webuser where email=?;";
-        $stmt = $database->prepare($sqlmain);
-        $stmt->bind_param("s",$email);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        if($result->num_rows==1){
-            $error='<label for="promter" class="form-label" style="color:rgb(255, 62, 62);text-align:center;">Already have an account for this Email address.</label>';
-        }else{
-            //TODO
-            $database->query("insert into patient(pemail,pname,ppassword, paddress, pnic,pdob,ptel) values('$email','$name','$newpassword','$address','$nic','$dob','$tele');");
-            $database->query("insert into webuser values('$email','p')");
-
-            //print_r("insert into patient values($pid,'$email','$fname','$lname','$newpassword','$address','$nic','$dob','$tele');");
-            $_SESSION["user"]=$email;
-            $_SESSION["usertype"]="p";
-            $_SESSION["username"]=$fname;
-
-            header('Location: patient/index');
-            $error='<label for="promter" class="form-label" style="color:rgb(255, 62, 62);text-align:center;"></label>';
-        }
-        
-    }else{
-        $error='<label for="promter" class="form-label" style="color:rgb(255, 62, 62);text-align:center;">Password Conformation Error! Reconform Password</label>';
-    }
-
-
-
-    
-}else{
-    //header('location: signup');
-    $error='<label for="promter" class="form-label"></label>';
-}
-
-?>
-
-
 <div class="row">
+  <div class="col-md-6 rounded-4 d-flex justify-content-center align-items-center flex-column left-box" style="background: #EAEFFF;">
+      <div class="featured-image mb-3">
+       <img src="img/cakit.png" class="img-fluid" style="width: 250px;">
+      </div>
+      <h1>Ayo Peduli Kesehatan Anda!</h1>
+      <p>Lakukan pemeriksaan terhadap penyakit yang Anda alami dengan melakukan booking dokter terlebih dahulu</p>
+  </div> 
+  
+  <div class="col-md-6 right-box">
+     <div class="row align-items-center">
+           <div class="header-text mb-4">
+           <a href="index.php"><img src="img/LogoSehati.png" alt=""></a>
+                <h2>Selamat Datang! 👋</h2>
+                <p>Silakan lengkapi data akun Anda</p>
+           </div>
+           
+           <form action="" method="POST">
+               <?php if (!empty($error)): ?>
+                   <tr>
+                       <td colspan="2">
+                           <div class="error-message" style="color: red; text-align: center;">
+                               <?php echo htmlspecialchars($error); ?>
+                           </div>
+                       </td>
+                   </tr>
+               <?php endif; ?>
 
-<!--------------------------- Left Box ----------------------------->
-   <div class="col-md-6 rounded-4 d-flex justify-content-center align-items-center flex-column left-box" style="background: #EAEFFF;">
-       <div class="featured-image mb-3">
-        <img src="img/cakit.png" class="img-fluid" style="width: 250px;">
-       </div>
-       <h1>Ayo Peduli Kesehatan Anda!</h1>
-       <p>Lakukan pemerikasaan terhadap penyakit yang Anda alami dengan melakukan booking dokter terlebih dahulu</p>
-   </div> 
-   
-<!-------------------- ------ Right Box ---------------------------->
+               <tr>
+                   <td class="label-td" colspan="2">
+                       <label for="newemail" class="form-label">Email: </label>
+                   </td>
+               </tr>
+               <tr>
+                   <td class="label-td" colspan="2">
+                       <input type="email" name="newemail" class="input-text" placeholder="Masukkan Alamat Email" required 
+                              value="<?php echo isset($email) ? htmlspecialchars($email) : ''; ?>">
+                   </td>
+               </tr>
+               <tr>
+                   <td class="label-td" colspan="2">
+                       <label for="tele" class="form-label">Nomor Telepon: </label>
+                   </td>
+               </tr>
+               <tr>
+                   <td class="label-td" colspan="2">
+                       <input type="tel" name="tele" class="input-text" placeholder="Contoh: 081212121212" 
+                              value="<?php echo isset($tele) ? htmlspecialchars($tele) : ''; ?>">
+                   </td>
+               </tr>
+               <tr>
+                   <td class="label-td" colspan="2">
+                       <label for="newpassword" class="form-label">Buat Password Baru: </label>
+                   </td>
+               </tr>
+               <tr>
+                   <td class="label-td" colspan="2">
+                       <input type="password" name="newpassword" class="input-text" placeholder="Password Baru" required>
+                   </td>
+               </tr>
+               <tr>
+                   <td class="label-td" colspan="2">
+                       <label for="cpassword" class="form-label">Konfirmasi Password: </label>
+                   </td>
+               </tr>
+               <tr>
+                   <td class="label-td" colspan="2">
+                       <input type="password" name="cpassword" class="input-text" placeholder="Konfirmasi Password" required>
+                   </td>
+               </tr>
     
-   <div class="col-md-6 right-box">
-      <div class="row align-items-center">
-            <div class="header-text mb-4">
-            <a href="index"><img src="img/LogoSehati.png" alt=""></a>
-                 <h2>Selamat Datang!👋</h2>
-                 <p>Silakan masukkan data diri Anda</p>
-            </div>
-            <tr>
-                <form action="" method="POST" >
-                <td class="label-td" colspan="2">
-                    <label for="newemail" class="form-label">Email: </label>
-                </td>
-            </tr>
-            <tr>
-                <td class="label-td" colspan="2">
-                    <input type="email" name="newemail" class="input-text" placeholder="Email Address" required>
-                </td>
-                
-            </tr>
-            <tr>
-                <td class="label-td" colspan="2">
-                    <label for="tele" class="form-label">Mobile Number: </label>
-                </td>
-            </tr>
-            <tr>
-                <td class="label-td" colspan="2">
-                    <input type="tel" name="tele" class="input-text"  placeholder="ex: 081212121212" >
-                </td>
-            </tr>
-            <tr>
-                <td class="label-td" colspan="2">
-                    <label for="newpassword" class="form-label">Create New Password: </label>
-                </td>
-            </tr>
-            <tr>
-                <td class="label-td" colspan="2">
-                    <input type="password" name="newpassword" class="input-text" placeholder="New Password" required>
-                </td>
-            </tr>
-            <tr>
-                <td class="label-td" colspan="2">
-                    <label for="cpassword" class="form-label">Confirm Password: </label>
-                </td>
-            </tr>
-            <tr>
-                <td class="label-td" colspan="2">
-                    <input type="password" name="cpassword" class="input-text" placeholder="Conform Password" required>
-                </td>
-            </tr>
-     
-            <tr>
-                
-                <td colspan="2">
-                    <?php echo $error ?>
-
-                </td>
-            </tr>
-            
-            <tr>
-                <td>
-                    <input type="reset" value="Reset" class="login-btn btn-primary-soft btn" >
-                </td>
-                <td>
-                    <input type="submit" value="Sign Up" class="login-btn btn-primary btn">
-                </td>
-
-            </tr>
-            
-            <tr>
-                <td colspan="2">
-                    <br>
-                    <label for="" class="sub-text" style="font-weight: 280;">Already have an account&#63; </label>
-                    <a href="login" class="hover-link1 non-style-link">Login</a>
-                    <br><br><br>
-                </td>
-            </tr>
-
-                    </form>
-            </tr>
-        </table>
-
-    </div>
+               <tr>
+                   <td>
+                       <input type="reset" value="Reset" class="login-btn btn-primary-soft btn">
+                   </td>
+                   <td>
+                       <input type="submit" value="Daftar" class="login-btn btn-primary btn">
+                   </td>
+               </tr>
+               
+               <tr>
+                   <td colspan="2">
+                       <br>
+                       <label for="" class="sub-text" style="font-weight: 280;">Sudah punya akun? </label>
+                       <a href="login.php" class="hover-link1 non-style-link">Login</a>
+                       <br><br><br>
+                   </td>
+               </tr>
+           </form>
+       </div>
+   </div>
 </body>
 </html>
--- signup.php
+++ signup.php
@@ -1,154 +1,151 @@
+<?php
+session_start();
+
+$_SESSION["user"] = "";
+$_SESSION["usertype"] = "";
+
+date_default_timezone_set('Asia/Kolkata');
+$date = date('Y-m-d');
+$_SESSION["date"] = $date;
+
+$error = '';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+   $fname = filter_var($_POST['fname'], FILTER_SANITIZE_STRING);
+   $lname = filter_var($_POST['lname'], FILTER_SANITIZE_STRING);
+   $address = filter_var($_POST['address'], FILTER_SANITIZE_STRING);
+   $nic = filter_var($_POST['nic'], FILTER_SANITIZE_STRING);
+   $dob = $_POST['dob'];
+
+   if (empty($fname)) {
+       $error = "Nama depan harus diisi";
+   }
+   elseif (empty($lname)) {
+       $error = "Nama belakang harus diisi";
+   }
+   elseif (empty($dob)) {
+       $error = "Tanggal lahir harus diisi";
+   }
+   else {
+       $_SESSION["personal"] = [
+           'fname' => $fname,
+           'lname' => $lname,
+           'address' => $address,
+           'nic' => $nic,
+           'dob' => $dob
+       ];
+
+       header("Location: create-account");
+       exit();
+   }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/animations.css">  
-    <link rel="stylesheet" href="css/main.css">  
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/signup.css">
-        
-    <title>Sign Up</title>
-    
-	<link rel="icon" href="img/LogoSehati.png">
-    
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="css/animations.css">  
+   <link rel="stylesheet" href="css/main.css">  
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+   <link rel="stylesheet" href="css/signup.css">
+       
+   <title>Sign Up</title>
+   
+   <link rel="icon" href="img/LogoSehati.png">
 </head>
 <body>
-<?php
-
-//learn from w3schools.com
-//Unset all the server side variables
-
-session_start();
-
-$_SESSION["user"]="";
-$_SESSION["usertype"]="";
-
-// Set the new timezone
-date_default_timezone_set('Asia/Kolkata');
-$date = date('Y-m-d');
-
-$_SESSION["date"]=$date;
-
-
-
-if($_POST){
-
-    
-
-    $_SESSION["personal"]=array(
-        'fname'=>$_POST['fname'],
-        'lname'=>$_POST['lname'],
-        'address'=>$_POST['address'],
-        'nic'=>$_POST['nic'],
-        'dob'=>$_POST['dob']
-    );
-
-
-    print_r($_SESSION["personal"]);
-    header("location: create-account");
-
-
-
-
-}
-
-?>
-
-
-    
 <div class="row">
+  <div class="col-md-6 rounded-4 d-flex justify-content-center align-items-center flex-column left-box" style="background: #EAEFFF;">
+      <div class="featured-image mb-3">
+       <img src="img/cakit.png" class="img-fluid" style="width: 250px;">
+      </div>
+      <h1>Ayo Peduli Kesehatan Anda!</h1>
+      <p>Lakukan pemerikasaan terhadap penyakit yang Anda alami dengan melakukan booking dokter terlebih dahulu</p>
+  </div> 
+  
+  <div class="col-md-6 right-box">
+     <div class="row align-items-center">
+           <div class="header-text mb-4">
+           <a href="index"><img src="img/LogoSehati.png" alt=""></a>
+                <h2>Selamat Datang!👋</h2>
+                <p>Silakan masukkan data diri Anda</p>
+           </div>
+           <form action="" method="POST">
+               <?php if (!empty($error)): ?>
+                   <tr>
+                       <td colspan="2">
+                           <div class="error-message" style="color: red; text-align: center;">
+                               <?php echo $error; ?>
+                           </div>
+                       </td>
+                   </tr>
+               <?php endif; ?>
+               <tr>
+                   <td class="label-td" colspan="2">
+                       <label for="name" class="form-label">Nama Lengkap: </label>
+                   </td>
+               </tr>
+               <tr>
+                   <td class="label-td">
+                       <input type="text" name="fname" class="input-text" placeholder="Masukan Nama Depan Anda" required value="<?php echo isset($fname) ? htmlspecialchars($fname) : ''; ?>">
+                   </td>
+                   <td class="label-td">
+                       <input type="text" name="lname" class="input-text" placeholder="Masukan Nama Belakang Anda" required value="<?php echo isset($lname) ? htmlspecialchars($lname) : ''; ?>">
+                   </td>
+               </tr>
+               <tr>
+                   <td class="label-td" colspan="2">
+                       <label for="address" class="form-label">Alamat: </label>
+                   </td>
+               </tr>
+               <tr>
+                   <td class="label-td" colspan="2">
+                       <input type="text" name="address" class="input-text" placeholder="Masukan Alamat Lengkap Anda" value="<?php echo isset($address) ? htmlspecialchars($address) : ''; ?>">
+                   </td>
+               </tr>
+               <tr>
+                   <td class="label-td" colspan="2">
+                       <label for="nic" class="form-label">NIK: </label>
+                   </td>
+               </tr>
+               <tr>
+                   <td class="label-td" colspan="2">
+                       <input type="text" name="nic" class="input-text" placeholder="Masukan NIK Anda" value="<?php echo isset($nic) ? htmlspecialchars($nic) : ''; ?>">
+                   </td>
+               </tr>
+               <tr>
+                   <td class="label-td" colspan="2">
+                       <label for="dob" class="form-label">Tanggal Lahir: </label>
+                   </td>
+               </tr>
+               <tr>
+                   <td class="label-td" colspan="2">
+                       <input type="date" name="dob" class="input-text" value="<?php echo isset($dob) ? htmlspecialchars($dob) : ''; ?>">
+                   </td>
+               </tr>
 
-<!--------------------------- Left Box ----------------------------->
-   <div class="col-md-6 rounded-4 d-flex justify-content-center align-items-center flex-column left-box" style="background: #EAEFFF;">
-       <div class="featured-image mb-3">
-        <img src="img/cakit.png" class="img-fluid" style="width: 250px;">
+               <tr>
+                   <td>
+                       <input type="reset" value="Reset" class="login-btn btn-primary-soft btn">
+                   </td>
+                   <td>
+                       <input type="submit" value="Next" class="login-btn btn-primary btn">
+                   </td>
+               </tr>
+               
+               <div class="akun">
+                   <td colspan="2">
+                       <br>
+                       <label for="" class="sub-text" style="font-weight: 280;">Sudah punya akun? </label>
+                       <a href="login" class="hover-link1 non-style-link">Login</a>
+                       <br><br><br>
+                   </td>
+               </div>
+           </form>
        </div>
-       <h1>Ayo Peduli Kesehatan Anda!</h1>
-       <p>Lakukan pemerikasaan terhadap penyakit yang Anda alami dengan melakukan booking dokter terlebih dahulu</p>
-   </div> 
-   
-<!-------------------- ------ Right Box ---------------------------->
-    
-   <div class="col-md-6 right-box">
-      <div class="row align-items-center">
-            <div class="header-text mb-4">
-            <a href="index"><img src="img/LogoSehati.png" alt=""></a>
-                 <h2>Selamat Datang!👋</h2>
-                 <p>Silakan masukkan data diri Anda</p>
-            </div>
-            <tr>
-                <form action="" method="POST" >
-                <td class="label-td" colspan="2">
-                    <label for="name" class="form-label">Nama Lengkap: </label>
-                </td>
-            </tr>
-            <tr>
-                <td class="label-td">
-                    <input type="text" name="fname" class="input-text" placeholder="Masukan Nama Depan Anda" required>
-                </td>
-                <td class="label-td">
-                    <input type="text" name="lname" class="input-text" placeholder="Masukan Nama Belakang Anda" required>
-                </td>
-            </tr>
-            <tr>
-                <td class="label-td" colspan="2">
-                    <label for="address" class="form-label">Alamat: </label>
-                </td>
-            </tr>
-            <tr>
-                <td class="label-td" colspan="2">
-                    <input type="text" name="address" class="input-text" placeholder="Masukan Alamat Lengkap Anda">
-                </td>
-            </tr>
-            <tr>
-                <td class="label-td" colspan="2">
-                    <label for="nic" class="form-label">NIK: </label>
-                </td>
-            </tr>
-            <tr>
-                <td class="label-td" colspan="2">
-                    <input type="text" name="nic" class="input-text" placeholder="Masukan NIK Anda">
-                </td>
-            </tr>
-            <tr>
-                <td class="label-td" colspan="2">
-                    <label for="dob" class="form-label">Date of Birth: </label>
-                </td>
-            </tr>
-            <tr>
-                <td class="label-td" colspan="2">
-                    <input type="date" name="dob" class="input-text">
-                </td>
-            </tr>
-            <tr>
-                <td class="label-td" colspan="2">
-                </td>
-            </tr>
-
-            <tr>
-                <td>
-                    <input type="reset" value="Reset" class="login-btn btn-primary-soft btn" >
-                </td>
-                <td>
-                    <input type="submit" value="Next" class="login-btn btn-primary btn">
-                </td>
-
-            </tr>
-            
-            <div class = "akun">
-                <td colspan="2">
-                    <br>
-                    <label for="" class="sub-text" style="font-weight: 280;">Already have an account&#63; </label>
-                    <a href="login" class="hover-link1 non-style-link">Login</a>
-                    <br><br><br>
-                </td>
-            </div>
-                    </form>
-            </tr>
-        </div>
-
-    </div>
+   </div>
 </body>
 </html>
